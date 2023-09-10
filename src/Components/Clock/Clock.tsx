59bd8ebf5c276497afeebff6a68f242c6208611f
John <calebmiles02415@gmail.com>
2023-09-10 13:45:18 -0700

    Complete Clock

--- src/Components/Clock/Clock.tsx
+++ src/Components/Clock/Clock.tsx
@@ -1,16 +1,37 @@
 import React from "react";
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
-const Clock = () => {
-    const [time, setTime] = useState(0);
+const Clock: React.FC = () => {
+
+    const [count, setCount] = useState<number>(0);
+    const [isRunning, setIsRunning] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (isRunning) {
+            const interval = setInterval(() => {
+                setCount(prevCount => prevCount + 1);
+            }, 1000);
+            return () => clearInterval(interval);
+        }
+    }, [isRunning]);
+
+    const start = () => setIsRunning(true);
+
+    const stop = () => setIsRunning(false);
+
+    const reset = () => setCount(0);
 
     return (
         <div>
-            <button className="">Start</button>
-            <button className="">Stop</button>
-            <button className="">Reset</button>
+            <div>
+                {Math.floor(count / 3600)} : {Math.floor(count / 60) % 60} : {count % 60}
+            </div>
+            <button onClick={start}>Start</button>
+            <button onClick={stop}>Stop</button>
+            <button onClick={reset}>Reset</button>
+
         </div>
     );
-}
+};
 
 export default Clock;
